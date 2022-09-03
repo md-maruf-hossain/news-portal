8ceb8b2a7dd0208b5a2dfeca9e29f35cfccaf104
marufhossain6470 <104669410+marufhossain6470@users.noreply.github.com>
2022-09-03 13:02:03 +0600

    dynamic data loaded by console.log

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-console.log('added added')
+// catagory
+const loadCatagories = () => {
+  const url = "https://openapi.programming-hero.com/api/news/categories";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCatagories(data.data.news_category));
+};
+
+function displayCatagories(catagories) {
+  // console.log(catagories)
+  const catagorySection = document.getElementById("catagory-section");
+  for (const catagory of catagories) {
+    const catagoryDiv = document.createElement("div");
+    catagoryDiv.innerHTML = `
+    <a class="nav-link" href="#" onclick="clickedCatagoryLoad('${catagory.category_id}')">${catagory.category_name}</a>
+    `;
+    catagorySection.appendChild(catagoryDiv);
+  }
+}
+
+const clickedCatagoryLoad = (newsId) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${newsId}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayClickedCatagory(data.data[0]));
+};
+
+const displayClickedCatagory = (clickedCatagories) => {
+  console.log(clickedCatagories);
+};
+
+loadCatagories();
+
