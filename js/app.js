8feb6dfa2d6905aa47ccb340736078d112eeb5b1
marufhossain6470 <104669410+marufhossain6470@users.noreply.github.com>
2022-09-03 15:26:49 +0600

    cards added

--- js/app.js
+++ js/app.js
@@ -22,12 +22,29 @@ const clickedCatagoryLoad = (newsId) => {
   const url = `https://openapi.programming-hero.com/api/news/category/${newsId}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayClickedCatagory(data.data[0]));
+    .then((data) => displayClickedCatagory(data.data));
 };
 
 const displayClickedCatagory = (clickedCatagories) => {
   console.log(clickedCatagories);
+  const clickedCatagoriesSection = document.getElementById("news-container");
+  clickedCatagoriesSection.textContent = "";
+  for (const clickedCatagory of clickedCatagories) {
+    const clickedCatagoriesDiv = document.createElement("div");
+    clickedCatagoriesDiv.classList.add("col");
+    clickedCatagoriesDiv.innerHTML = `
+    <div class="card">
+        <img src="${clickedCatagory.image_url}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${clickedCatagory.title.length > 30 ? clickedCatagory.title.slice(0, 40) + "..." : clickedCatagory.title}</h5>
+            <p class="card-text">${clickedCatagory.details.length > 30 ? clickedCatagory.details.slice(0, 100) + "..." : clickedCatagory.details}</p>
+              <div>
+
+              </div>
+          </div>
+      </div>
+    `;
+    clickedCatagoriesSection.appendChild(clickedCatagoriesDiv);
+  }
 };
-
 loadCatagories();
-
